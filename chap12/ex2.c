9cb26d2394acbea4e2ace3c516f07d6220caebe6
sh1n00 <rinjyou0031@toki.waseda.jp>
2024-03-25 00:42:28 +0900

    add: chap12

--- chap12/ex2.c
+++ chap12/ex2.c
@@ -5,6 +5,17 @@
 void main(int argc, char *argv[]) {
   for(;;) {
     fputs(PROMPT, stdout);
-    
+        
   }
 }
+
+static void* xmalloc(size_t size) {
+  void *p;
+  p = malloc(size);
+  if(p == NULL) {
+    perror("malloc");
+    exit(1);
+  }
+  return p;
+}
+
